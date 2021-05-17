1a942723ecbd3f38063da6d3957bb172c02f3152
Amauri Junior <junior_18ribeiro@hotmail.com>
2021-05-17 17:10:02 -0400

    start of reset password

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import authenticateRoutes from './authenticate.routes';
+import { passwordRoutes } from './password.routes';
 import usersRouter from './users.routes';
 
 const router = Router();
 
 router.use('/users', usersRouter);
 router.use(authenticateRoutes);
+router.use('/password', passwordRoutes);
 
 export default router;
